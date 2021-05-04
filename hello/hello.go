57ff74ce52ecd9910e4802a8e0ffedb79a6c755e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-04 00:28:12 -0300

    chore: read user input & list menu

--- hello/hello.go
+++ hello/hello.go
@@ -15,4 +15,15 @@ func main() {
 	fmt.Println("The type of var name is:", reflect.TypeOf(name))
 	fmt.Println("The type of var age is:", reflect.TypeOf(age))
 	fmt.Println("The type of var version is:", reflect.TypeOf(version))
+
+	fmt.Println("1 - Start Monitoring")
+	fmt.Println("2 - Show Logs")
+	fmt.Println("0 - Exit")
+
+	var command int
+	// fmt.Scanf("%d", &command)
+	fmt.Scan(&command)
+	fmt.Println("Command address: ", &command)
+	fmt.Println("Command: ", command)
+
 }
