b174cafab4f25a0b3aa9816e39262f67af4290ce
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-04 00:12:18 -0300

    chore: var types

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!!")
+	var name string = "Filipe"
+	var age int
+	var version float32 = 1.1
+	fmt.Println("Hi,", name)
+	fmt.Println("age:", age)
+	fmt.Println("This app is in version,", version)
 }
