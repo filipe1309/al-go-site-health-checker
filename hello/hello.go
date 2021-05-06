2810fc5a6ee16eebd00cce6a07a0102ee4a35ad2
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-06 01:07:58 -0300

    chore: show log

--- hello/hello.go
+++ hello/hello.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"reflect"
@@ -28,6 +29,7 @@ func main() {
 			initMonitoring()
 		case 2:
 			fmt.Println("Showing Logs...")
+			showLogs()
 		case 0:
 			fmt.Println("Exiting...")
 			os.Exit(0)
@@ -140,4 +142,12 @@ func registerLog(site string, status bool) {
 	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	file.Close()
+}
+
+func showLogs() {
+	file, err := ioutil.ReadFile("hello/log.txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(string(file))
 }
